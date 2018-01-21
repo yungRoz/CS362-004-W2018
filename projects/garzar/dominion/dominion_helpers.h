056de7db6a648954b5a9e7dd91817a2a5e8791d3
Romano Garza <garzar@flip3.engr.oregonstate.edu>
2018-01-20 18:06:26 -0800

    Add protos for refactor methods

--- projects/garzar/dominion/dominion_helpers.h
+++ projects/garzar/dominion/dominion_helpers.h
@@ -11,5 +11,11 @@ int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3, 
 	       struct gameState *state, int handPos, int *bonus);
+int adventurerEffect(int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus); 
+int remodelEffect(int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus); 
+int smithyEffect(int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus);
+int villageEffect(int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus);
+
+int baronEffect(int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus);
 
 #endif
